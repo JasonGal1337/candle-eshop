5e36fe5df06d0ac68f7516ab5cc221b89f2438d3
JasonGal <jason.gal.bu@gmail.com>
2025-08-19 11:55:40 +0300

    fixed a pathing error in admin & user router

--- server/routers/adminRouter.js
+++ server/routers/adminRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const controller = require("../controllers/adminController");
+const { validate } = require("../validate");
+const { adminSignupSchema, adminLoginSchema, adminVerifySchema } = require("../validators/adminValidator");
 
 const router = express.Router();
 
-router.post("/signup", controller.signup);
-router.post("/login", controller.login);
-router.post("/verify", controller.verify);
+router.post("/signup", validate(adminSignupSchema), controller.signup);
+router.post("/login",  validate(adminLoginSchema),  controller.login);
+router.post("/verify", validate(adminVerifySchema), controller.verify);
 
 module.exports = router;
